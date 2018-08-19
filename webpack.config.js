3f46c55354bce2462ffa502bf1e3962f65ece479
LoeiFy <LoeiFy@gmail.com>
2018-08-19 17:17:00 +0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,35 @@
-const { NODE_ENV } = process.env
-const dev = NODE_ENV === 'development'
-const umd = NODE_ENV === 'production'
-const demo = NODE_ENV === 'demo'
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-let output = {
-  library: 'puffin',
-  libraryTarget: umd ? 'umd' : 'commonjs2',
-  filename: 'puffin.js',
-  path: `${__dirname}/${umd ? 'dist' : 'lib'}`,
+const { NODE_ENV: mode } = process.env
+const template = (type) => {
+  const cdn = `<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/react/16.4.2/umd/react.production.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/react-dom/16.4.2/umd/react-dom.production.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.js"></script>
+  `
+
+  return `<!doctype html>
+<html>
+<head>
+  <title> Puffin </title>
+  <link href="//cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.css" rel="stylesheet">
+</head>
+<body style="width: 100vw; height: 100vh;">
+  <div style="height: 100%;" id="root"></div>
+</body>
+${type === 'demo' ? cdn : ''}
+</html>
+  `
 }
 
-if (dev) {
-  output = { filename: 'puffin.js' }
-}
+const base = {
+  mode: 'development',
 
-if (demo) {
-  output = {
-    filename: 'index.js',
-    path: `${__dirname}/docs`,
-  }
-}
+  entry: './entry/index.js',
 
-module.exports = {
-  mode: dev ? 'development' : 'production',
+  output: { filename: 'index.js' },
 
-  entry: dev || demo ? './entry/index.js' : './entry/entry.js',
-
-  output,
-
-  resolve: {
-    extensions: ['.js'],
-  },
+  resolve: { extensions: ['.js'] },
 
   devServer: {
     disableHostCheck: true,
@@ -72,11 +71,87 @@ module.exports = {
     ],
   },
 
-  devtool: dev ? 'cheap-module-eval-source-map' : 'source-map',
-
-  externals: dev ? {} : {
-    react: umd || demo ? 'React' : 'commonjs react',
-    'react-dom': umd || demo ? 'ReactDOM' : 'commonjs reactDom',
-    antd: umd || demo ? 'antd' : 'commonjs antd',
-  },
+  devtool: 'cheap-module-eval-source-map',
 }
+
+if (mode === 'umd' || mode === 'demo') {
+  base.mode = 'production'
+}
+
+if (mode === 'umd' || mode === 'commonjs') {
+  base.entry = './entry/entry.js'
+}
+
+if (mode === 'demo') {
+  base.output = {
+    filename: '[name].[chunkhash:8].js',
+    path: `${__dirname}/docs`,
+  }
+}
+
+if (mode === 'umd') {
+  base.output = {
+    library: 'puffin',
+    libraryTarget: 'umd',
+    filename: 'index.js',
+    path: `${__dirname}/dist}`,
+  }
+}
+
+if (mode === 'commonjs') {
+  base.output = {
+    libraryTarget: 'commonjs2',
+    filename: 'index.js',
+    path: `${__dirname}/lib`,
+  }
+}
+
+if (mode === 'umd' || mode === 'demo') {
+  base.devtool = 'source-map'
+}
+
+if (mode === 'commonjs') {
+  base.devtool = false
+}
+
+if (mode === 'umd' || mode === 'demo') {
+  base.externals = {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    antd: 'antd',
+  }
+}
+
+if (mode === 'commonjs') {
+  base.externals = {
+    react: 'commonjs react',
+    'react-dom': 'commonjs reactDom',
+    antd: 'commonjs antd',
+  }
+}
+
+if (mode === 'dev') {
+  base.plugins = [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      templateContent: template(mode),
+    }),
+  ]
+}
+
+if (mode === 'demo') {
+  base.plugins = [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      templateContent: template(mode),
+      minify: {
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+        collapseWhitespace: true,
+      },
+    }),
+  ]
+}
+
+module.exports = base

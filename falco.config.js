f17f4ea3f14522ddc71d4e3995c61ffe591dfbed
LoeiFy <LoeiFy@gmail.com>
2019-03-02 21:05:30 +0800

    chore: use falco

---
+++ falco.config.js
@@ -0,0 +1,29 @@
+const { NODE_ENV } = process.env
+
+module.exports = {
+  externals: [
+    {
+      name: 'react',
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      // urls: 'https://cdn.staticfile.org/react/16.8.2/umd/react.development.js',
+      urls: 'https://cdn.staticfile.org/react/16.8.2/umd/react.production.min.js',
+    },
+    {
+      name: 'react-dom',
+      root: 'ReactDOM',
+      urls: 'https://cdn.staticfile.org/react-dom/16.8.2/umd/react-dom.production.min.js',
+      // urls: 'https://cdn.staticfile.org/react-dom/16.8.2/umd/react-dom.development.js',
+    },
+  ],
+  registry: 'https://registry.npm.taobao.org',
+  mode: NODE_ENV ? 'production' : 'development',
+  output: NODE_ENV === 'prod'
+    ? {
+      library: 'nycticorax',
+      libraryTarget: 'umd',
+    }
+    : {},
+}
